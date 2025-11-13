ec4ace214481b12ccf4afe8f9295ae6a183cbf88
Origami-Tobiichi <cyberdark454@gmail.com>
2025-11-13 22:04:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const http = require('http');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 443;
 
 // Middleware untuk parsing
 app.use(express.json());
